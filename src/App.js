31db11f6c0fbb9cb8c88e2ccffbca23013622fd7
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-11-06 22:57:06 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function Login() {
   const navigate = useNavigate();
   const supabaseUrl = 'https://ifshyzgwiukjqupmrwde.supabase.co'
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlmc2h5emd3aXVranF1cG1yd2RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDY1NDMsImV4cCI6MjA0NjQyMjU0M30.Q8YYezFhpYX-4mNvk7NyVseoH8SPNkyF8mlwUwYoD3w'
+  // eslint-disable-next-line no-unused-vars
   const supabase = createClient(supabaseUrl, supabaseKey);
   module.exports = { supabase };
     const handleLogin = async () => {
