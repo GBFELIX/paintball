8535496c4a83fa20dfa2a608fadcdaeef64c9145
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-11-09 14:27:34 -0300

    update

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors({
 
 }));
+
 app.use(bodyParser.json());
 
 
@@ -537,7 +538,7 @@ db.on('error', (err) => {
   }
 });
 
-const PORT = process.env.PORT ||  3306;
+const PORT = process.env.PORT ||  5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function Login() {
 
     const handleLogin = async () => {
       try {
-        const response = await fetch(`https://api.lapaintball.com.br/login`, { 
+        const response = await fetch(`http://localhost:5000/login`, { 
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
