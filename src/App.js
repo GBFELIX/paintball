58509c07cb5e7fd5d49c6d5c8b5f75a258a8da59
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-11-09 15:24:12 -0300

    up

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function Login() {
 
     const handleLogin = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/login`, { 
+        const response = await fetch(`/.netlify/functions/login`, { 
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- src/pages/StatusGamer.js
+++ src/pages/StatusGamer.js
@@ -3,93 +3,94 @@ import VendaAvulsa from './Componentes/VendaAvul';
 import CardJog from './Componentes/Cardjog';
 import CardDespesas from './Componentes/CardDespesas';
 import { useNavigate } from 'react-router-dom';
+// icons
 import { FaPlus } from "react-icons/fa6";
 
 export default function StatusGame() {
-  const [jogo, setJogo] = useState({});
-  const [jogadores, setJogadores] = useState([{ 
-    nome: '', 
-    numero: '1', 
-    items: [], 
-    selectedItem: '', 
-    isClosed: false 
-  }]);
-  const [vendasAvulsas, setVendasAvulsas] = useState([{ 
-    nome: '', 
-    numero: '1', 
-    items: [], 
-    selectedItem: '', 
-    isClosed: false 
-  }]);
-  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
-  const [despesas, setDespesas] = useState([{ 
-    nome: '', 
-    numero: '1', 
-    items: [], 
-    selectedItem: '', 
-    isClosed: false 
-  }]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const storedData = localStorage.getItem('dataJogo');
-    const storedHora = localStorage.getItem('horaJogo');
-  
-    if (storedData) {
-      setJogo({ data: storedData, hora: storedHora });
-    }
-  }, []);
-
-  const handleAddJogador = () => {
-    const newNumero = (jogadores.length + 1).toString();
-    setJogadores([...jogadores, { 
-      nome: '', 
-      numero: newNumero, 
-      items: [], 
-      selectedItem: '', 
-      isClosed: false 
+    const [jogo, setJogo] = useState({});
+    const [jogadores, setJogadores] = useState([{
+        nome: '',
+        numero: '1',
+        items: [],
+        selectedItem: '',
+        isClosed: false
     }]);
-  };
-
-  const handleAddVendaAvulsa = () => {
-    const newNumero = (vendasAvulsas.length + 1).toString();
-    setVendasAvulsas([...vendasAvulsas, { 
-      nome: '', 
-      numero: newNumero, 
-      items: [], 
-      selectedItem: '', 
-      isClosed: false 
+    const [vendasAvulsas, setVendasAvulsas] = useState([{
+        nome: '',
+        numero: '1',
+        items: [],
+        selectedItem: '',
+        isClosed: false
     }]);
-  };
-  
-  const handleAddDespesa = () => {
-    const newNumero = (despesas.length + 1).toString();
-    setDespesas([...despesas, { 
-      nome: '', 
-      numero: newNumero, 
-      items: [], 
-      selectedItem: '', 
-      isClosed: false 
+    const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+    const [despesas, setDespesas] = useState([{
+        nome: '',
+        numero: '1',
+        items: [],
+        selectedItem: '',
+        isClosed: false
     }]);
-  };
+    const navigate = useNavigate();
 
-  const handleClosePedido = (index) => {
-    const updatedJogadores = [...jogadores];
-    updatedJogadores[index].isClosed = !updatedJogadores[index].isClosed;
-    setJogadores(updatedJogadores);
-  };
+    useEffect(() => {
+        const storedData = localStorage.getItem('dataJogo');
+        const storedHora = localStorage.getItem('horaJogo');
 
-  const jogadoresAtivos = jogadores.filter(jogador => !jogador.isClosed).length;
-  const jogadoresInativos = jogadores.filter(jogador => jogador.isClosed).length;
+        if (storedData) {
+            setJogo({ data: storedData, hora: storedHora });
+        }
+    }, []);
 
-  const handleFecharPartida = () => {
-    setShowConfirmationModal(true);
-  };
+    const handleAddJogador = () => {
+        const newNumero = (jogadores.length + 1).toString();
+        setJogadores([...jogadores, {
+            nome: '',
+            numero: newNumero,
+            items: [],
+            selectedItem: '',
+            isClosed: false
+        }]);
+    };
 
-  const confirmCloseGame = () => {
-    setShowConfirmationModal(false);
-    navigate('/resumogame');
-  };
+    const handleAddVendaAvulsa = () => {
+        const newNumero = (vendasAvulsas.length + 1).toString();
+        setVendasAvulsas([...vendasAvulsas, {
+            nome: '',
+            numero: newNumero,
+            items: [],
+            selectedItem: '',
+            isClosed: false
+        }]);
+    };
+
+    const handleAddDespesa = () => {
+        const newNumero = (despesas.length + 1).toString();
+        setDespesas([...despesas, {
+            nome: '',
+            numero: newNumero,
+            items: [],
+            selectedItem: '',
+            isClosed: false
+        }]);
+    };
+
+    const handleClosePedido = (index) => {
+        const updatedJogadores = [...jogadores];
+        updatedJogadores[index].isClosed = !updatedJogadores[index].isClosed;
+        setJogadores(updatedJogadores);
+    };
+
+    const jogadoresAtivos = jogadores.filter(jogador => !jogador.isClosed).length;
+    const jogadoresInativos = jogadores.filter(jogador => jogador.isClosed).length;
+
+    const handleFecharPartida = () => {
+        setShowConfirmationModal(true);
+    };
+
+    const confirmCloseGame = () => {
+        setShowConfirmationModal(false);
+        navigate('/resumogame');
+    };
 
   const cancelCloseGame = () => {
     setShowConfirmationModal(false);
@@ -97,6 +98,7 @@ export default function StatusGame() {
   
   return (
     <section className="bg-black text-white min-h-screen w-full h-auto rounded-md p-3 flex flex-col gap-4">
+    <section className="bg-black text-white w-full h-auto rounded-md p-3 flex flex-col gap-4">
       <div className="flex justify-between w-full gap-4 mb-4">
         <div className="flex flex-col items-start">
           <p className="font-semibold">Data da Partida</p>
@@ -112,6 +114,7 @@ export default function StatusGame() {
         </div>
       </div>
 
+      {/* Componentes VendaAvulsa, CardJog e CardDespesas */}
       <div className="flex flex-wrap gap-4 text-black">
         <CardJog 
           jogadores={jogadores} 
@@ -132,35 +135,38 @@ export default function StatusGame() {
           handleClosePedido={handleClosePedido}
         />
       </div>
+    </section>
+    <div className="flex justify-end mt-auto">
+    
+    <button // botão para adicionar jogador
+        onClick={handleAddJogador}
+        className="bg-primary hover:bg-white duration-300 m-2 w-20 h-20 rounded-full flex justify-center items-center"
+      >
+        <FaPlus size={30} />
+      </button>
+      <button // botão para adicionar venda avulsa
+        onClick={handleAddVendaAvulsa}
+        className="bg-blue-600 hover:bg-white duration-300 m-2 w-20 h-20 rounded-full flex justify-center items-center"
+      >
+        <FaPlus size={30} />
+      </button>
+      <button 
+        onClick={handleAddDespesa}
+        className="bg-red-600 hover:bg-white duration-300 m-2 w-20 h-20 rounded-full flex justify-center items-center"
+      >
+        <FaPlus size={30} />
+      </button>
 
-      <div className="flex justify-end mt-auto">
-        <button 
-          onClick={handleAddJogador}
-          className="bg-primary hover:bg-white duration-300 m-2 w-20 h-20 rounded-full flex justify-center items-center"
-        >
-          <FaPlus size={30} />
-        </button>
-        <button 
-          onClick={handleAddVendaAvulsa}
-          className="bg-blue-600 hover:bg-white duration-300 m-2 w-20 h-20 rounded-full flex justify-center items-center"
-        >
-          <FaPlus size={30} />
-        </button>
-        <button 
-          onClick={handleAddDespesa}
-          className="bg-red-600 hover:bg-white duration-300 m-2 w-20 h-20 rounded-full flex justify-center items-center"
-        >
-          <FaPlus size={30} />
-        </button>
 
         <button 
-          onClick={handleFecharPartida}
+          onClick={handleFecharPartida}  // Abre o modal de confirmação
           className="mt-4 mb-4 rounded-md w-[150px] h-[40px] bg-primary flex justify-center items-center hover:bg-red-500 transition-colors"
         >
           <p>Fechar Partida</p>
         </button>
       </div>
 
+      {/* Modal de confirmação */}
       {showConfirmationModal && (
         <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded-lg w-96">
@@ -168,13 +174,13 @@ export default function StatusGame() {
             <div className="flex justify-between mt-4">
               <button
                 className="bg-gray-500 hover:bg-black text-white py-2 px-4 rounded-lg"
-                onClick={cancelCloseGame}
+                onClick={cancelCloseGame}  // Fecha o modal sem redirecionar
               >
                 Cancelar
               </button>
               <button
                 className="bg-black hover:bg-primary py-2 px-4 rounded-lg text-white"
-                onClick={confirmCloseGame}
+                onClick={confirmCloseGame}  // Confirma o fechamento e redireciona
               >
                 OK
               </button>
