80ce0e8132388aeb3618866fcd43efac7dfd99ac
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-11-18 18:25:56 -0300

    up

--- src/pages/Componentes/VendaAvul.js
+++ src/pages/Componentes/VendaAvul.js
@@ -96,27 +96,14 @@ const VendaAvul = ({ vendas, setVendas, handleAddVendaAvulsa }) => {
     };
 
     const handleClosePedido = (index) => {
-        const venda = vendas[index];
-
-        if (!venda.nome || venda.nome.trim() === '') {
-            toast.error('O nome da venda é obrigatório antes de fechar o pedido.', {
-                position: "top-right",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                theme: "light",
-            });
-            return;
-        }
-
-        if (venda.isClosed) {
-            const updatedVendas = [...vendas];
+        const updatedVendas = [...vendas];
+        if (updatedVendas[index].isClosed) {
             updatedVendas[index].isClosed = false;
             updatedVendas[index].items = [];
-            setVendas(updatedVendas);
+            updateVendas(updatedVendas);
         } else {
+            const valorTotal = updatedVendas[index].items.reduce((sum, item) => sum + (parseFloat(item.valor) || 0), 0);
+            setValorTotalVendaAtual(valorTotal);
             setVendaIndexForPayment(index);
             setShowPaymentModal(true);
         }
