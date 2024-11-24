98d0b64bb3f3b1ba719e01fb3b46c65fd2e3d1b0
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-11-24 00:29:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -101,16 +101,14 @@ function Login() {
   };
 
   return (
-    <div className='w-full h-screen bg-black flex items-center justify-center'>
-       <nav className="bg-black">
+    <div className='w-full h-screen flex flex-col items-center justify-center bg-black'>
+    <nav className="bg-black">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <img src={logo} className="h-8" alt="LA PaintBall" />
-        
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            
             <li>
-              <Link to="#" className="text-white hover:text-primary duration-150">Administradores</Link>
+              <Link to="/loginjog" className="text-white hover:text-primary duration-150">Administradores</Link>
             </li>
             <li>
               <button onClick={handleLogout} className="text-white hover:text-primary duration-150">
