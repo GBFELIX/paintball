3d2067cb6a161e6450f154bf2ad46c18bcdd4c1c
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-12-07 07:50:22 -0300

    Update Loginjog.js

--- src/pages/Loginjog.js
+++ src/pages/Loginjog.js
@@ -87,7 +87,7 @@ function Loginjog() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <img src={logo} className="h-8" alt="LA PaintBall" />
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-black">
               <li>
                 <Link to="/loginjog" className="text-white hover:text-primary duration-150">Administradores</Link>
               </li>
