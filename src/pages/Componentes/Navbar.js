d38c06d152d2486d866166f8d40e247497d7769c
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-12-07 07:26:03 -0300

    Update Navbar.js

--- src/pages/Componentes/Navbar.js
+++ src/pages/Componentes/Navbar.js
@@ -19,7 +19,7 @@ function NavBar() {
 
   return (
     <>
-      <nav className="bg-black dark:bg-black">
+      <nav className="bg-black dark:bg-black bg-gray-800">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <img src={logo} className="h-8" alt="LA PaintBall" />
           <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
