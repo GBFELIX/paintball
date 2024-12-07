5c23a3cee2c28528a467b80e7307b2a579a469e3
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-12-07 14:24:39 -0300

    Update Loginjog.js

--- src/pages/Loginjog.js
+++ src/pages/Loginjog.js
@@ -90,7 +90,6 @@ function Loginjog() {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-black">
               <li>
                 <button onClick={handleLogout} className="text-white hover:text-primary duration-150">
-                  Sair do sistema
                 </button>
               </li>
             </ul>
