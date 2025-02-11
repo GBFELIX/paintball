e8c55f15c4e13d60a2228919d4c23e369a6516d8
Gabriel <felixgabriel.sant@gmail.com>
2025-02-10 22:57:48 -0300

    att

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -241,7 +241,7 @@ const Game = () => {
                             <div className="p-2 flex flex-col justify-center items-center">
                                 <p><strong>Forma de Pagamento:</strong></p>
                                 {console.log('Forma de Pagamento:', jogador.formaPagamento)}
-                                <p>{Array.isArray(jogador.formaPagamento) && jogador.formaPagamento.length > 0 ? jogador.formaPagamento.join(' e ') : 'N/A'}</p>
+                                <p>{Array.isArray(jogador.formaPagamento) || jogador.formaPagamento.length > 0 ? jogador.formaPagamento.join(' e ') : 'N/A'}</p>
                             </div>
                             <p><strong>Valor Total:</strong> R$ {jogador.valor_total || '0'}</p>
                         </div>
