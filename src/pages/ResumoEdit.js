b038ae76c485681f87cdba3f6dfe13eea81f8bba
Gabriel <felixgabriel.sant@gmail.com>
2025-03-09 20:12:26 -0300

    att

--- src/pages/ResumoEdit.js
+++ src/pages/ResumoEdit.js
@@ -9,54 +9,13 @@ export default function ResumoEdit() {
     const [jogo, setJogo] = useState({});
     const [pagamentos, setPagamentos] = useState([]);
     const [formasPagamento, setFormasPagamento] = useState([]);
-    const [totalAvulso, setTotalAvulso] = useState(0);
-    const [despesas, setDespesas] = useState([]);
 
     useEffect(() => {
-        const storedData = localStorage.getItem('dataJogo');
-        const storedHora = localStorage.getItem('horaJogo');
-        const storedTotalAvulso = localStorage.getItem('totalAvulso');  
-        
-        if (storedTotalAvulso) {
-            setTotalAvulso(parseFloat(storedTotalAvulso));
-        }
-
-        if (storedData) {
-            setJogo({ data: storedData, hora: storedHora });
-        }
-
-        const pagamentosArmazenados = JSON.parse(localStorage.getItem('pagamentos')) || [];
-        setPagamentos(pagamentosArmazenados);
-
-        const totais = pagamentosArmazenados.reduce((acc, pagamento) => {
-            const valor = parseFloat(pagamento.valorTotal);
-
-            acc[pagamento.formaPagamento] = (acc[pagamento.formaPagamento] || 0) + valor;
-            return acc;
-        }, {
-            credito: 0,
-            debito: 0,
-            dinheiro: 0,
-            avulso: 0,
-            pix: 0, 
-            deposito: 0,
-        });
-
-        setFormasPagamento(totais);
-
-        const despesasArmazenadas = JSON.parse(localStorage.getItem('despesas')) || [];
-        setDespesas(despesasArmazenadas);
-
         const dataFromGame = location.state || {};
-        console.log(dataFromGame);
-
-        setJogo({
-            data: dataFromGame.dataJogo,
-            hora: dataFromGame.horaJogo,
-        });
-
+        setJogo({ data: dataFromGame.dataJogo, hora: dataFromGame.horaJogo });
         setPagamentos(dataFromGame.pagamentos || []);
         setFormasPagamento(dataFromGame.formaPagamento || []);
+        console.log(dataFromGame);
     }, [location.state]);
 
     const imprimirResumo = () => {
@@ -81,18 +40,13 @@ export default function ResumoEdit() {
     };
 
     const fecharPartida = () => {
-        const totalArrecadado = Object.values(formasPagamento).reduce((acc, val) => acc + val, 0) + totalAvulso;
-        const totalDespesas = despesas.reduce((acc, despesa) => acc + parseFloat(despesa.valorTotal), 0);
-        const valortot = totalArrecadado - totalDespesas;
+        const totalArrecadado = Object.values(formasPagamento).reduce((acc, val) => acc + val, 0);
         const dataFinanceira = {
             dataJogo: jogo.data,
             horaJogo: jogo.hora,
             totalJogadores: pagamentos.length,
             formasPagamento,
-            totalAvulso,
             totalArrecadado,
-            totalDespesas,
-            valortot
         };
         axios.put('./.netlify/functions/api-financeiro', dataFinanceira)
         .then(() => {
@@ -120,11 +74,9 @@ export default function ResumoEdit() {
         });
         
         localStorage.removeItem('pagamentos');
-        localStorage.removeItem('totalAvulso');
         localStorage.removeItem('dataJogo');
         localStorage.removeItem('horaJogo');
         localStorage.removeItem('itensVendaAvul'); 
-        localStorage.removeItem('despesas'); 
         
         navigate('/estoque');
     };
@@ -158,24 +110,6 @@ export default function ResumoEdit() {
                         ) : (
                             <p>Nenhuma forma de pagamento disponível</p>
                         )}
-                        <div className="flex flex-row justify-around items-start">
-                            <p className="text-xl font-semibold">Despesas</p>
-                            <p id="despesas">
-                                R$ {despesas.reduce((acc, despesa) => acc + (parseFloat(despesa.valorTotal) || 0), 0).toFixed(2)}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid grid-flow-row md:grid-cols-2 gap-2 mt-3">
-                    <div className="bg-[#1D0C82] rounded-md w-full h-30 flex flex-col justify-center items-center py-14">
-                        <h1 className="text-white text-2xl font-bold">Valor Total</h1>
-                        <h2 id="valorPartida" className="text-primary text-3xl font-semibold">R${Object.values(formasPagamento).reduce((acc, val) => acc + val, 0).toFixed(2)}</h2>
-                    </div>
-                    <div className="bg-[#1D0C82] rounded-md w-full h-30 flex flex-col justify-center items-center py-14">
-                        <h1 className="text-white text-2xl font-bold">Total Arrecadado</h1>
-                        <h2 id="valortot" className="text-primary text-3xl font-semibold">
-                            R${(Object.values(formasPagamento).reduce((acc, val) => acc + val, 0) - despesas.reduce((acc, despesa) => acc + parseFloat(despesa.valorTotal), 0)).toFixed(2)}
-                        </h2>
                     </div>
                 </div>
                 <div className="grid grid-flow-row md:grid-cols-2 p-2 gap-2">
