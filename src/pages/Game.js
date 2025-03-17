c0d5261a1fb9aabca7e134cbe9129631e0c5cd9b
Gabriel <felixgabriel.sant@gmail.com>
2025-03-17 15:58:44 -0300

    att

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -363,7 +363,7 @@ const Game = () => {
         // Redireciona para a página ResumoEdit com os dados
         navigate('/resumoedit', { state: dataToSend });
     };
-
+    const valorTotalVenda = jogador.items.reduce((sum, item) => sum + (parseFloat(item.valor) * (item.quantidade || 1) || 0), 0);
     return (
         <div className="bg-black text-white min-h-screen w-full h-auto rounded-md p-3 flex flex-col gap-4">
             <div className="flex justify-between w-full gap-4 mb-4">
@@ -379,7 +379,7 @@ const Game = () => {
                 </div>
             </div>
             <div className="flex flex-wrap gap-4 text-black">
-                {jogadores.map((jogador, index) => (
+                {jogadores.map((jogador, index) => (      
                     <section key={index} className={`w-[300px] h-auto rounded-lg bg-white ${jogador.isClosed ? 'opacity-50 pointer-events-none' : ''}`}>
                         <header className="bg-primary w-full p-3 rounded-t-lg text-black font-normal flex justify-between items-center">
                             <h3 className="text-lg font-semibold ml-2">{jogador.nome_jogador || 'Despesa'}</h3>
