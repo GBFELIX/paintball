a470ddbe5c872262a0360948f5d077c3279bc5d8
Gabriel <felixgabriel.sant@gmail.com>
2025-03-20 13:52:12 -0300

    att

--- src/pages/Financeiro.js
+++ src/pages/Financeiro.js
@@ -146,9 +146,6 @@ export default function Financeiro() {
     navigate('/game', { state: { dataJogo, horaJogo } });
   };
 
-  const totalArrecadado = financeiroData.reduce((acc, item) => acc + (item.total_arrecadado || 0), 0);
-  const totalDespesas = financeiroData.reduce((acc, item) => acc + (item.despesas || 0), 0);
-
   return (
     <section className="bg-black p-4 w-full h-screen flex flex-col items-center overflow-auto"> 
       <ToastContainer />
@@ -285,20 +282,6 @@ export default function Financeiro() {
                 ))}
             </tbody>
           </table>
-          <div className="w-full flex justify-between px-3 py-5 mt-5 border-t border-gray-300">
-            <div className="w-1/3 text-center">
-              <p className="text-black text-lg font-semibold">Valor Total Arrecadado:</p>
-              <p className="text-red-500 text-lg font-bold">R$ {totalArrecadado.toFixed(2)}</p>
-            </div>
-            <div className="w-1/3 text-center">
-              <p className="text-black text-lg font-semibold">Valor Total de Despesas:</p>
-              <p className="text-red-500 text-lg font-bold">R$ {totalDespesas.toFixed(2)}</p>
-            </div>
-            <div className="w-1/3 text-center">
-              <p className="text-black text-lg font-semibold">Total Geral:</p>
-              <p className="text-red-500 text-lg font-bold">R$ {(totalArrecadado - totalDespesas).toFixed(2)}</p>
-            </div>
-          </div>
         </div>
       )}
 
