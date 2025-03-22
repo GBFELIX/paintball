789d29b65c8d8dfda34e0f65af3244c5e0a0553f
Gabriel <felixgabriel.sant@gmail.com>
2025-03-21 23:13:20 -0300

    att

--- src/pages/Financeiro.js
+++ src/pages/Financeiro.js
@@ -311,16 +311,16 @@ export default function Financeiro() {
           </table>
           <div className="w-full flex justify-between px-3 py-5 mt-5 border-t border-gray-300">
             <div className="w-1/3 text-center">
-              <p className="text-black text-lg font-semibold">Valor Total Arrecadado:</p>
-              <p className="text-red-500 text-lg font-bold">R$ {totalArrecadado.toFixed(2)}</p>
+              <p className="text-primary text-lg font-semibold">Valor Total Arrecadado:</p>
+              <p className="text-red-500 text-lg font-bold">R$ {(totalArrecadado || 0).toFixed(2)}</p>
             </div>
             <div className="w-1/3 text-center">
-              <p className="text-black text-lg font-semibold">Valor Total de Despesas:</p>
-              <p className="text-red-500 text-lg font-bold">R$ {totalDespesas.toFixed(2)}</p>
+              <p className="text-primary text-lg font-semibold">Valor Total de Despesas:</p>
+              <p className="text-red-500 text-lg font-bold">R$ {(totalDespesas || 0).toFixed(2)}</p>
             </div>
             <div className="w-1/3 text-center">
-              <p className="text-black text-lg font-semibold">Total Geral:</p>
-              <p className="text-red-500 text-lg font-bold">R$ {(totalArrecadado - totalDespesas).toFixed(2)}</p>
+              <p className="text-primary text-lg font-semibold">Total Geral:</p>
+              <p className="text-red-500 text-lg font-bold">R$ {((totalArrecadado || 0) - (totalDespesas || 0)).toFixed(2)}</p>
             </div>
           </div>
         </div>
